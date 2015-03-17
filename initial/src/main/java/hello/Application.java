2546c41fed3eec137f1ef7f5ac5c809312097955
dn200978lak <dn200978lak@gmail.com>
2015-03-17 15:15:51 +0200

    0.0.0

---
+++ initial/src/main/java/hello/Application.java
@@ -0,0 +1,48 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Aleksandr Konstantinovitch
+ * @version 1.0
+ * @date 17/02/2015
+ * {@link http://spring-projects.ru/guides/caching/}
+ * {@link https://spring.io/guides/gs/caching/}
+ */
+
+@Configuration
+@EnableAutoConfiguration
+public class Application {
+
+    private final static Logger log = LoggerFactory.getLogger(Application.class);
+
+    @Configuration
+    static class Runner implements CommandLineRunner {
+        @Autowired
+        private BookRepository bookRepository;
+
+        @Override
+        public void run(String... args) throws Exception {
+            log.info(".... Fetching books");
+            log.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234"));
+            log.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234"));
+            log.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234"));
+        }
+    }
+
+    @Bean
+    public BookRepository bookRepository() {
+        return new SimpleBookRepository();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
